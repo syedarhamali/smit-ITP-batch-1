4dd2a8f52098730d9370040a8484bee7d2be3025
Arham~ <arhamali63@gmail.com>
2024-12-19 16:13:27 +0500

    Add files via upload

---
+++ js-class-1/index.js
@@ -0,0 +1,11 @@
+// javascript is loosly type programming language 
+
+// in javascript , variable is a container use to store data 
+
+// in  javascript , types are defined dynamic on development 
+
+var myName = "Arham"
+
+myName = 2
+
+alert("Welcome to my website")
---
+++ js-class-2/index.js
@@ -0,0 +1,35 @@
+// alert("welcome"); // statement
+// parantheses
+
+
+// var numberOne = "Arham";
+// var numberTwo = 1;
+
+// var result = numberOne + numberTwo;
+
+// agr apke pass ik value string k andar he or ik number k andar he to wo usko calculate krly ga except plus (+) k 
+// misal k toor per app 1 string den or 1 number den to result 11 ayega (concatention kehlata he ye)
+
+// alert(result); 
+
+
+// myName = 2000
+// var myName = "Arham"
+
+// string hamesha "" me ayengi app single quotation '' or double quotations ""
+
+
+// alert(myName); 
+
+var totalMarks = 500
+
+var subOne = prompt("Enter your name")
+var subTwo = prompt("Enter your name")
+var userName = prompt("Enter your name")
+var userName = prompt("Enter your name")
+var userName = prompt("Enter your name")
+
+var result = subOne +  subTwo 
+
+var percentage = totalMarks  / result / 100
+alert("Welcome to my website " + userName)
