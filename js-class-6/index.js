45bc6469615a264b4e97c6413c9f88ebe6fb6435
Arham~ <arhamali63@gmail.com>
2025-01-09 15:53:20 +0500

    class-js-6

---
+++ js-class-6/index.js
@@ -0,0 +1,52 @@
+// var numberArray = [1,2,3,4,5]
+
+// numberArray.push(6)
+
+// numberArray.splice(2,0,10, 11 , 12 , 13 ,14)
+
+// var cities = ['Karachi' , 'Islamabad' , 'Lahore' , 'Peshawar']
+
+// cities.splice(1,0 , "Sialkot")
+
+// console.log(cities)
+
+// var todoList = []
+
+// var userValue = prompt("Enter Todo")
+
+// todoList.push(userValue)
+
+// console.log(todoList)
+
+// var num1 = 6
+// var num2 = 5
+
+// var result = num1 + num2
+
+// console.log(result)
+
+// var num1 = 7
+// var num2 = 9
+
+// var result = num1 + num2
+
+// console.log(result)
+
+// var userNumberOne = prompt("Enter your first Number");
+// var userNumberTwo = +prompt("Enter your second Number");
+
+// function sumOfTwoValue(x, y) {
+//   var result = x + y;
+
+//   alert(result);
+// }
+
+// sumOfTwoValue(userNumberOne, userNumberTwo);
+
+// function sumOfTwoValue() {
+//   var userNumberOne = +prompt("Enter your first Number");
+//   var userNumberTwo = +prompt("Enter your second Number");
+//   var result = userNumberOne + userNumberTwo;
+
+//   alert(result);
+// }
