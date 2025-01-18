bd1fe5c26e16b728efcb4886ecc97da1566affb2
Arham~ <arhamali63@gmail.com>
2025-01-18 16:06:47 +0500

    class-code-10

---
+++ js-class-10/index.js
@@ -0,0 +1,33 @@
+const firstName = ["Arham", "Taha", "Moiz", "Abubakar", "Ayaan"]; // we cannot change the const value
+
+var lastName = ["Ali", "Khan", "Sheikh"];
+
+var fullNames = [];
+// console.log(i) // undefined
+
+// dono array ko merge krwana he or new array bnana he fullName ka
+
+// debugger
+// for (var i = 0; i < firstName.length; i++) {
+//   for (var j = 0; j < lastName.length; j++) {
+//     fullNames.push(firstName[j] + " " + lastName[j]);
+//     console.log(fullNames)
+//   }
+// }
+
+var firstNames = ["BlueRay ", "Upchuck ", "Lojack ", "Gizmo ", "Do-Rag "];
+var lastNames = ["Zzz", "Burp", "Dogbone", "Droop"];
+var fullNames = [];
+
+for (var i = 1; i <= 100; i = i + 10) {
+  for (var j = i; j < i + 10; j++) {
+    document.write(j + " ");
+  }
+  document.write("<br />");
+}
+
+// console.log(fullNames);
+
+// var firstName = "kajsdbhfjkas"
+
+// console.log(firstName)
